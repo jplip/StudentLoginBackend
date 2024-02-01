1517918da8069fed3ad931a1655ef7a2b9440ed3
jplip <jpliporada@gmail.com>
2024-02-01 10:40:38 -0800

    revert conflict to my code, delete token for now

--- model/users.py
+++ model/users.py
@@ -157,7 +157,7 @@ class User(db.Model):
 
     # CRUD update: updates user name, password, phone
     # returns self
-    def update(self, name="", uid="", password="",  exercise = "", tracking="", dob=""):
+    def update(self, name="", uid="", password="",  exercise = "", tracking="", coins="", dob=""):
         """only updates values with length"""
         if len(name) > 0:
             self.name = name
@@ -169,6 +169,8 @@ class User(db.Model):
             self.exercise = exercise
         if len(tracking) > 0:
             self.tracking = tracking 
+        if coins > 0:
+            self.coins = coins
         if dob is not None:
             self.dob = dob    
         db.session.commit()
@@ -214,6 +216,7 @@ def initUsers():
                     dob=user_data['dob'],
                     tracking=user_data['tracking'],
                     exercise=user_data['exercise'],
+                    coins=user_data['coins']
                 )
             else:
                 # Proceed with inserting the new user
